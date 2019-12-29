3655de20d6ba23fe547fd8b4fa38c71f791d6f95
Jie Zhou <jzhou41@cs.rochester.edu>
2019-12-29 16:34:45 -0500

    Renamed _MMSafe_ptr to _TS_ptr.
    _TS_ptr is shorter and so easier to type.

--- tests/include/safe_mm_checked.h
+++ tests/include/safe_mm_checked.h
@@ -1,6 +1,6 @@
 /*
  * This files provides a customized memory allocator and a deallocator
- * for struct objects pointed by a _MMSafe_ptr<T>.
+ * for struct objects pointed by a _TS_ptr<T>.
  * */
 #ifndef _SAFE_MM_CHECKED_H
 #define _SAFE_MM_CHECKED_H
@@ -10,11 +10,11 @@
 #include <stdlib.h>
 
 // A helper struct that has the "same" inner structure as a mmsafe_ptr.
-// It is used to help create a _MMSafe_ptr.
+// It is used to help create a _TS_ptr.
 typedef struct {
   void *p;
   uint64_t ID;
-} _MMSafe_ptr_Rep;
+} _TS_ptr_Rep;
 
 uint64_t key = 1;
 
@@ -22,12 +22,12 @@ uint64_t key = 1;
 // Function: mmsafe_alloc()
 //
 // This is a customized memory allocator. First it creates a helper struct
-// _MMSafe_ptr_Rep and initializes its two field accordingly. Second it casts
-// the type of an implicit pointer to the type of a pointer to a _MMSafe_ptr.
-// Finally it returns the dereference of the pointer to _MMSafe_ptr.
+// _TS_ptr_Rep and initializes its two field accordingly. Second it casts
+// the type of an implicit pointer to the type of a pointer to a _TS_ptr.
+// Finally it returns the dereference of the pointer to _TS_ptr.
 //
 __attribute__ ((noinline))
-for_any(T) mmsafe_ptr<T> mmsafe_alloc(unsigned long struct_size) {
+for_any(T) tsptr<T> mmsafe_alloc(unsigned long struct_size) {
     void *raw_ptr = malloc(struct_size);
 
     // Generate a random number as the ID.
@@ -39,9 +39,9 @@ for_any(T) mmsafe_ptr<T> mmsafe_alloc(unsigned long struct_size) {
     *((uint64_t *)(raw_ptr)) = new_ID;
 
     // Create a helper struct.
-    _MMSafe_ptr_Rep safe_ptr = { .p = raw_ptr, .ID = new_ID };
+    _TS_ptr_Rep safe_ptr = { .p = raw_ptr, .ID = new_ID };
 
-    mmsafe_ptr<T> *mmsafe_ptr_ptr = (mmsafe_ptr<T> *)&safe_ptr;
+    tsptr<T> *mmsafe_ptr_ptr = (tsptr<T> *)&safe_ptr;
     return *mmsafe_ptr_ptr;
 }
 
@@ -51,12 +51,12 @@ for_any(T) mmsafe_ptr<T> mmsafe_alloc(unsigned long struct_size) {
 // This is a customized memory deallocator. It sets the ID of the struct to 0
 // and calls free() from the stdlib to free the struct.
 //
-// @param p - a _MMSafe_ptr whose pointee is going to be freed.
+// @param p - a _TS_ptr whose pointee is going to be freed.
 //
-for_any(T) void mmsafe_free(mmsafe_ptr<T> p) {
+for_any(T) void mmsafe_free(tsptr<T> p) {
   // Without the "volatile" keyword, Clang may optimize away the next
   // statement.
-  volatile _MMSafe_ptr_Rep *mmsafe_ptr_ptr = (_MMSafe_ptr_Rep *)&p;
+  volatile _TS_ptr_Rep *mmsafe_ptr_ptr = (_TS_ptr_Rep *)&p;
 
   // This step may not be necessary in some cases. In some implementation,
   // free() zeros out all bytes of the memory region of the freed object.
