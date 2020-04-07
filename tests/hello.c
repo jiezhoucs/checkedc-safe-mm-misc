8e619b7ee1c38a53899b955c630430af204b019f
Jie Zhou <jzhou41@cs.rochester.edu>
2020-04-07 16:33:08 -0400

    Updated the hello.c as a simple template.

--- tests/hello.c
+++ tests/hello.c
@@ -1,22 +1,15 @@
+#include "header.h"
+
+void foo();
+
 #if 0
-#include <stdio.h>
-#endif
-
-#include "safe_mm_checked.h"
-
-typedef struct {
-    uint64_t ID;
-    int val;
-    long long_val;
-    float f;
-} Data;
-
 int main(int argc, char *argv[]) {
-    mmsafe_ptr<Data> data_ptr = mmsafe_alloc<Data>(sizeof(Data));
-
-    (*data_ptr).val = 10;
-
-    mmsafe_free<Data>(data_ptr);
+    foo();
 
     return 0;
+}
+#endif
+
+void foo() {
+
 }
