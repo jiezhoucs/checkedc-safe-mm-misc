a14596f5c9031696f9dbcb330e4e4829315b3799
Jie Zhou <jzhou41@cs.rochester.edu>
2020-09-15 15:49:04 -0400

    Miscellaenous updates on the func.c test files.
    - Added basic tests for passing and returning mm_array_ptr.
    - Replaced error message printing function perror() with print_error().

--- tests/func.c
+++ tests/func.c
@@ -3,7 +3,7 @@
  * */
 
 #include <stdbool.h>
-#include "inc.h"
+#include "debug.h"
 
 //
 // Test using a mm_ptr of function parameter.
@@ -19,7 +19,7 @@ void f0(mm_ptr<Data> p, bool free_it) {
     (*p).d = 3.14;
 
     if (p->i != 10 || p->j != 20 || p->d != 3.14) {
-        perror("ERROR: Testing parameter passing failed in function f0().\n");
+        print_error("Testing parameter passing failed in function f0()");
     }
 }
 
@@ -35,7 +35,7 @@ void f1() {
     f0(p, false);
 
     if (p->i != 10 || p->j != 20 || p->d != 3.14) {
-        perror("ERROR: Testing parameter passing failed in function f1().\n");
+        print_error("Testing parameter passing failed in function f1()");
     }
 
     // Free the struct in the callee.
@@ -51,7 +51,7 @@ resume:
 // Test using multiple MM_ptr passed by function parameter.
 void f2(mm_ptr<Data> p0, mm_ptr<Data> p1) {
     if (p0->i != p1->i || p0->l != p1->l || p0->d != p1->d) {
-        perror("ERROR: Testing parameter passing failed in function f2().\n");
+        print_error("Testing parameter passing failed in function f2()");
     }
 }
 
@@ -78,12 +78,7 @@ void f3() {
 
 //
 // Test returning an MM_ptr from a function.
-//
-// Here we add a "int i"parameter but doesn't use it because the
-// current Checked C requires that
-// "function with no prototype cannot have a return type that is a checked type"
-//  We may need relex this restriction for MM_ptr.
-mm_ptr<Data> f4(int i) {
+mm_ptr<Data> f4() {
     mm_ptr<Data> p = mm_alloc<Data>(sizeof(Data));
     p->i = 10;
     p->l = 20;
@@ -92,16 +87,69 @@ mm_ptr<Data> f4(int i) {
     return p;
 }
 
+//
+// Test using mm_ptr as a function's return value.
 void f5() {
     print_start("return an MM_ptr from a function");
 
-    mm_ptr<Data> p = f4(0);
+    mm_ptr<Data> p = f4();
 
     if (p->i != 10 || p->l != 20 || p->d != 3.14) {
-        perror("ERROR: Testing parameter passing failed in function f1().\n");
+        print_error("Testing parameter passing failed in function f5()");
     }
 
     print_end("return an MM_ptr from a function");
+}
+
+//
+// Test using _MM_array_ptr as a function parameter.
+void f6(mm_array_ptr<int> p, bool free_it) {
+    if (free_it) mm_array_free<int>(p);
+
+    if (setjmp(resume_context) == 1) goto resume;
+
+    if (p[0] != 42)
+        print_error("func.c::f6(): test using mm_array_ptr as func paramter failed");
+    p[0] = 84;
+
+resume:
+    print_end("testing using a _MM_array_ptr as a function parameter");
+}
+
+//
+// Test passing mm_array_ptr to a function and checking if the pointee's change
+// in the callee are visial in the caller.
+void f7() {
+    mm_array_ptr<int> p = mm_array_alloc<int>(sizeof(int) * 10);
+    p[0] = 42;
+    f6(p, false);
+    if (p[0] != 84)
+        print_error("func.c::f7(): test passing mm_array_ptr to function failed");
+
+    p[0] = 42;
+    f6(p, true);
+}
+
+//
+// Test returning an _MM_array_ptr from a function.
+mm_array_ptr<int> f8() {
+    mm_array_ptr<int> p = mm_array_alloc<int>(sizeof(int) * 10);
+    for (int i = 0; i < 10; i++) p[i] = i;
+
+    return p;
+}
+
+//
+// Test using mm_array_ptr as a function's return value.
+void f9() {
+    mm_array_ptr<int> p = f8();
+    for (int i = 0; i < 10; i++) {
+        if (p[i] != i)
+            print_error("func.c::f9(): test using an mm_array_ptr as a function's\
+                    return value failed");
+    }
+
+    print_end("test using mm_array_ptr as a function's return value");
 }
 
 int main(int argc, char *argv[]) {
@@ -113,6 +161,10 @@ int main(int argc, char *argv[]) {
     f3();
 
     f5();
+
+    f7();
+
+    f9();
 
     print_main_end(__FILE__);
     return 0;
