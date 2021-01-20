d584e524ea4d7c26421d01997934b485714a5ae2
Jie Zhou <jzhou41@cs.rochester.edu>
2021-01-20 11:33:52 -0500

    Wrote a script to configure and compile thttpd.

---
+++ scripts/thttpd_build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env  bash
+
+#
+# This script generates the Makefile for thttpd.
+#
+# $1 - (optional) "baseline" for baseline, otherwise for the checked version.
+# $2 - (optional) "make" to compile thttpd.
+#
+
+# load common directory paths and variables
+. common.sh
+
+#
+# Decide if it's for the baseline or the checked version.
+if [[ $1 == "baseline" ]]; then
+    SRC_DIR=$MISC_DIR/benchmarks/baseline/thttpd-2.29
+    BUILD_DIR=$ROOT_DIR/benchmark-build/baseline/thttpd
+else
+    SRC_DIR=$MISC_DIR/benchmarks/checked/thttpd-2.29
+    BUILD_DIR=$ROOT_DIR/benchmark-build/checked/thttpd
+fi
+
+#
+# Run configure.sh to generate Makefile
+#
+configure() {
+    cd $SRC_DIR
+
+    export CC="$CC"
+
+    ./configure --prefix="$BUILD_DIR"
+}
+
+#
+# Compile thttpd
+#
+compile() {
+    cd $SRC_DIR
+    if [[ -f "Makefile" ]]; then
+        make -j4
+    else
+        echo "Makefile not found. Please first generate the Makefile."
+        exit
+    fi
+}
+
+#
+# Entrance of this script
+#
+configure
+
+if [[ $2 == "make" ]]; then
+    compile
+fi
