3598a349432d191bace9eab67aa7f58d41cd1bc0
Jie Zhou <jzhou41@cs.rochester.edu>
2021-05-22 16:13:37 -0400

    [CETS] Added scripts to help run Olden benchmarks with CETS.
    Basically copied and slightly modified corresponding files for the
    Checked C compiler.

--- scripts/common.sh
+++ scripts/common.sh
@@ -29,8 +29,9 @@ CC_RELEASE="$LLVM_RELEASE_BIN_DIR/clang"
 CETS_ROOT=$ROOT_DIR/cets
 CETS_BIN=$CETS_ROOT/build/bin
 CETS_TS_BUILD=$CETS_ROOT/ts-build
-CETS_LIB=$CETS_ROOT/lib
+CETS_LIB=$CETS_ROOT/llvm/libsoftboundcets
 CETS_CC=$CETS_BIN/clang
+CETS_LIT="$CETS_BIN/llvm-lit"
 # CETS_CXX=$CETS_BIN/clang++  // not supported
 
 #
