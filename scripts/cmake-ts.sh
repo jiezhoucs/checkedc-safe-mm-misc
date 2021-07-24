6a99ae65bf08bc703daa7a3442c98fbd2014d5d5
Jie Zhou <jzhou41@cs.rochester.edu>
2021-07-23 20:09:58 -0400

    [Olden] Updated the script that configures cmake for Checked Olden.
    Updated the script with LTO related stuffs.

--- scripts/cmake-ts.sh
+++ scripts/cmake-ts.sh
@@ -8,14 +8,25 @@
 # Load the common paths and variables.
 . common.sh
 
+# Use lld to link the libsafemm.
+CFLAGS="-fuse-ld=lld"
+LDFLAGS="$CHECKEDC_LIB/libsafemm.a"
+
 # Go to the build directory. Create one if it does not exist.
 [[ -d $TESTSUITE_BUILD_DIR ]] || mkdir -p $TESTSUITE_BUILD_DIR
 cd "$TESTSUITE_BUILD_DIR"
 rm -rf CMakeCache.txt
 
+if [[ $1 == "lto" ]]; then
+    CFLAGS="-flto $CFLAGS"
+    LDFLAGS="$CHECKEDC_LIB/libsafemm_lto.a"
+fi
+
 cmake -DCMAKE_C_COMPILER="$CC"                                                 \
+      -DCMAKE_C_FLAGS="$CFLAGS"                                                \
+      -DCMAKE_CXX_FLAGS="-mllvm -checkedc-init=false"                          \
       -DCMAKE_PREFIX_PATH="$CHECKEDC_INC;$CHECKEDC_LIB"                        \
-      -DCMAKE_SHARED_LINKER_FLAGS="rdynamic" \
+      -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"                                      \
       -C"$TESTSUITE_DIR"/cmake/caches/O3.cmake                                 \
       -DLARGE_PROBLEM_SIZE=1                                                   \
       "$TESTSUITE_DIR"
