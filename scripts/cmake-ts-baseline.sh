1faf39622b5fe840a9b743d4e95a694359eecb5f
Jie Zhou <jzhou41@cs.rochester.edu>
2021-07-31 22:19:18 -0400

    [Olden] Added LTO related options in the cmake script for baseline Olden

--- scripts/cmake-ts-baseline.sh
+++ scripts/cmake-ts-baseline.sh
@@ -11,12 +11,24 @@
 TESTSUITE_ORIGIN_DIR="$TESTS_DIR/test-suite-baseline"
 TESTSUITE_ORIGIN_BUILD_DIR="$TESTS_DIR/ts-build-baseline"
 
+# Use lld to link the libsafemm.
+CFLAGS="-fuse-ld=lld"
+LDFLAGS="$CHECKEDC_LIB/libsafemm.a"
+
 # Go to the build directory. Create one if it does not exist.
 [[ -d $TESTSUITE_ORIGIN_BUILD_DIR ]] || mkdir -p $TESTSUITE_ORIGIN_BUILD_DIR
 cd "$TESTSUITE_ORIGIN_BUILD_DIR"
 rm -rf CMakeCache.txt
 
+if [[ $1 == "lto" ]]; then
+    CFLAGS="-flto $CFLAGS"
+    LDFLAGS="-flto"
+fi
+
 cmake -DCMAKE_C_COMPILER="$CC_VANILLA"                                         \
+      -DCMAKE_C_FLAGS="$CFLAGS"                                                \
+      -DCMAKE_CXX_FLAGS="$CFLAGS"                                              \
+      -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"                                      \
       -C"$TESTSUITE_ORIGIN_DIR"/cmake/caches/O3.cmake                          \
       -DEXTRA_LARGE_PROBLEM_SIZE=1                                             \
       "$TESTSUITE_ORIGIN_DIR"
