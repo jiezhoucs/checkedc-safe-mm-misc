6bfd7ca4446a30fc7b38dbbddf155e25d48f2c61
Jie Zhou <jzhou41@cs.rochester.edu>
2021-08-15 16:44:42 -0400

    Commented off a few spatial Checked C keywords.
    To avoid conflicts in the source code of some test programs.

--- include/stdchecked.h
+++ include/stdchecked.h
@@ -1,11 +1,13 @@
 #ifndef __STDCHECKED_H
 #define __STDCHECKED_H
 
-/* #define ptr _Ptr */
-/* #define array_ptr _Array_ptr */
-/* #define nt_array_ptr _Nt_array_ptr */
+#if 0
+#define ptr _Ptr
+#define array_ptr _Array_ptr
+#define nt_array_ptr _Nt_array_ptr
 #define checked _Checked
 #define nt_checked _Nt_checked
+#endif
 #define mm_ptr _MM_ptr
 #define mm_array_ptr _MM_array_ptr
 #define unchecked _Unchecked
