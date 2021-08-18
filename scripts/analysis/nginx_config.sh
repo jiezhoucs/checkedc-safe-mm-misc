5b374b84ef562e2df09ba14975d6874591bc2ca0
Jie Zhou <jzhou41@cs.rochester.edu>
2021-08-18 15:37:53 -0400

    [nginx] Updated the script to configure the Makefiles for nginx.

--- scripts/analysis/nginx_config.sh
+++ scripts/analysis/nginx_config.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 #
-# This scripts configures and invokes the configure file of nginx
+# This scripts configures the compiling files of nginx.
 #
 
 . common.sh
 
 SRC_DIR="$PROGRAMS/nginx-1.21.1"
 
-export CC="$CC"
-export CFLAGS="-mllvm -get-obj-size"
+CC="$CC"
+CFLAGS="-flto"
+LDFLAGS="-fuse-ld=lld -Wl,-mllvm,-get-obj-size"
 
 cd "$SRC_DIR"
 ./configure --prefix="$SRC_DIR/build"                                          \
-            --without-http_rewrite_module
+            --with-cc="$CC"                                                    \
+            --with-cc-opt="$CFLAGS"                                            \
+            --with-ld-opt="$LDFLAGS"                                           \
+            --without-http_rewrite_module                                      \
 
 # Compile it if the first argument is "make".
 if [[ $1 == "make" ]]; then
