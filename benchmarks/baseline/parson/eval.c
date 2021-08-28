a6f0dba860684b5d23eb84a7d3d7a65f9103eba5
Jie Zhou <jzhou41@cs.rochester.edu>
2021-08-28 17:17:54 -0400

    [parson] Updated parson tests to process one input file for one run.
    The old tests parse all input files in one process. We updated the
    tests to parse one file in one process.

--- benchmarks/baseline/parson/eval.c
+++ benchmarks/baseline/parson/eval.c
@@ -15,6 +15,8 @@
 #include "parson.h"
 
 #define LARGE_FILE
+#define EVAL_ALL
+#undef EVAL_ALL
 
 #define TEST(A) printf("%d %-72s-", __LINE__, #A);\
                 if(A){puts(" OK");tests_passed++;}\
@@ -27,26 +29,6 @@
 #define JSON_FILE_NUM 16
 #define BILLION 1000000000
 
-/* JSON data files */
-static char *products_json         = "products";           /* 2.8K */
-static char *students_json         = "students";           /* 35K  */
-static char *grades_json           = "grades";             /* 92K  */
-static char *countries_small_json  = "countries-small";    /* 329K */
-static char *profiles_json         = "profiles";           /* 454K */
-static char *covers_json           = "covers";             /* 470K */
-static char *books_json            = "books";              /* 525K */
-static char *albums_json           = "albums";             /* 634K */
-static char *restaurant_json       = "restaurant";         /* 666K */
-static char *countries_big_json    = "countries-big";      /* 2.3M */
-static char *zips_json             = "zips";               /* 3.1M */
-static char *images_json           = "images";             /* 9.2M */
-static char *city_inspections_json = "city_inspections";   /* 24M  */
-static char *companies_json        = "companies";          /* 75M  */
-static char *citylots_json         = "citylots";           /* 181M */
-static char *trades_json           = "trades";             /* 232M */
-const char *data_files[JSON_FILE_NUM];
-static uint64_t exe_times[JSON_FILE_NUM];
-
 const char* get_file_path(const char *filename);
 
 static int tests_passed;
@@ -56,35 +38,7 @@ static int tests_failed;
 static struct timespec timer_start;
 static struct timespec timer_end;
 
-/*
- * 1. Put all data file names in an array.
- * 2. Initialize the array of execution time for each file.
- * */
-void prepare_files() {
-    for (unsigned i = 0; i < JSON_FILE_NUM; i++) {
-        data_files[i] = NULL;
-        exe_times[i] = 0;
-    }
-
-    data_files[0] = products_json;
-    data_files[1] = students_json;
-    data_files[2] = grades_json;
-    data_files[3] = countries_small_json;
-    data_files[4] = profiles_json;
-    data_files[5] = covers_json;
-    data_files[6] = books_json;
-    data_files[7] = albums_json;
-    data_files[8] = restaurant_json;
-    data_files[9] = countries_big_json;
-    data_files[10] = zips_json;
-    data_files[11] = images_json;
-    data_files[12] = city_inspections_json;
-    data_files[13] = companies_json;
-#ifdef LARGE_FILE
-    data_files[14] = citylots_json;   /* A large file */
-    data_files[15] = trades_json;     /* A large file */
-#endif
-}
+static uint64_t exe_time;
 
 /*
  * Construct the path of each data file.
@@ -97,90 +51,87 @@ const char* get_file_path(const char *filename) {
     return path_buf;
 }
 
-/*
- * Write results to a file.
- * */
-void write_results() {
-    const char *result_file_path = "../../../eval/perf_data/parson/baseline/result.csv";
-    FILE *perf_file = fopen(result_file_path,  "w+");
-    if (perf_file == NULL) {
+void file_open_status(FILE *file) {
+    if (file == NULL) {
         perror("failed to open result file");
         exit(errno);
     }
+}
 
-    fprintf(perf_file, "file,exe_time\n");
-    for (unsigned i = 0; i < JSON_FILE_NUM; i++) {
-        const char *file_name = data_files[i];
-        if (!file_name) continue;
-        fprintf(perf_file, "%s,%lu\n", file_name, exe_times[i]);
+/*
+ * Write results to a file.
+ * */
+void write_result(const char *file_name) {
+    const char *file_path = "../../../eval/perf_data/parson/baseline/result.csv";
+    FILE *perf_file = fopen(file_path, "r");
+    if (perf_file == NULL) {
+        perf_file = fopen(file_path, "w+");
+        file_open_status(perf_file);
+        fprintf(perf_file, "file_name,exe_time\n");
+    }  else {
+        file_open_status(perf_file = fopen(file_path, "a"));
     }
+    fprintf(perf_file, "%s,%lu\n", file_name, exe_time);
 }
 
 /*
  * The main body of the evaluation.
  * */
-void eval() {
-    for (unsigned i = 0; i < JSON_FILE_NUM; i++) {
-        if (data_files[i] == NULL) continue;
-
-        const char *file_path = get_file_path(data_files[i]);
-        if (!file_path) {
-            printf("Cannot find data file %s.\n", data_files[i]);
-            exit(1);
-        }
-        printf("JSONing %s...\n", data_files[i]);
-
-        if (clock_gettime(CLOCK_MONOTONIC, &timer_start) == -1) {
-            perror("clock_gettime() failed");
-            exit(errno);
-        }
-
-        /* Basic parsing and serialization */
-        JSON_Value *val = json_parse_file(file_path);
-        char *serialized = NULL, *serialized_pretty = NULL;
-        TEST(val != NULL);
-        /* Test json_value_get_type() */
-        val = json_parse_file(file_path);
-        TEST(json_value_get_type(val) == JSONObject);
-        serialized = json_serialize_to_string(val);
-        TEST(json_value_equals(json_parse_string(serialized), val));
-        serialized_pretty = json_serialize_to_string_pretty(val);
-        TEST(json_value_equals(json_parse_string(serialized_pretty), val));
-        json_free_serialized_string(serialized);
-        json_free_serialized_string(serialized_pretty);
-        if (val) { json_value_free(val); }
-
-        /* Parsing files with comments */
-        TEST((val = json_parse_file_with_comments(file_path)) != NULL);
-        /* Test json_value_get_type() */
-        val = json_parse_file(file_path);
-        TEST(json_value_get_type(val) == JSONObject);
-        TEST(json_value_equals(json_parse_string(
-                        serialized = json_serialize_to_string(val)), val));
-        TEST(json_value_equals(json_parse_string(
-                        serialized_pretty = json_serialize_to_string_pretty(val)), val));
-        json_free_serialized_string(serialized);
-        json_free_serialized_string(serialized_pretty);
-        if (val) { json_value_free(val); }
-
-        /* Finished parsing; record time. */
-        if (clock_gettime(CLOCK_MONOTONIC, &timer_end) == -1) {
-            perror("clock_gettime() failed");
-            exit(errno);
-        }
-        uint64_t elapsed = BILLION * (timer_end.tv_sec - timer_start.tv_sec) +
-            (timer_end.tv_nsec - timer_start.tv_nsec);
-        exe_times[i] = elapsed;
-        printf("elapsed time = %ld ns\n", elapsed);
+void eval(const char *file_name) {
+    const char *file_path = get_file_path(file_name);
+    if (!file_path) {
+        printf("Cannot find data file %s.\n", file_name);
+        exit(1);
     }
+    printf("Processing %s...\n", file_name);
+
+    if (clock_gettime(CLOCK_MONOTONIC, &timer_start) == -1) {
+        perror("clock_gettime() failed");
+        exit(errno);
+    }
+
+    /* Basic parsing and serialization */
+    JSON_Value *val = json_parse_file(file_path);
+    char *serialized = NULL, *serialized_pretty = NULL;
+    TEST(val != NULL);
+    /* Test json_value_get_type() */
+    val = json_parse_file(file_path);
+    TEST(json_value_get_type(val) == JSONObject);
+    serialized = json_serialize_to_string(val);
+    TEST(json_value_equals(json_parse_string(serialized), val));
+    serialized_pretty = json_serialize_to_string_pretty(val);
+    TEST(json_value_equals(json_parse_string(serialized_pretty), val));
+    json_free_serialized_string(serialized);
+    json_free_serialized_string(serialized_pretty);
+    if (val) { json_value_free(val); }
+
+    /* Parsing files with comments */
+    TEST((val = json_parse_file_with_comments(file_path)) != NULL);
+    /* Test json_value_get_type() */
+    val = json_parse_file(file_path);
+    TEST(json_value_get_type(val) == JSONObject);
+    TEST(json_value_equals(json_parse_string(
+                    serialized = json_serialize_to_string(val)), val));
+    TEST(json_value_equals(json_parse_string(
+                    serialized_pretty = json_serialize_to_string_pretty(val)), val));
+    json_free_serialized_string(serialized);
+    json_free_serialized_string(serialized_pretty);
+    if (val) { json_value_free(val); }
+
+    /* Finished parsing; record time. */
+    if (clock_gettime(CLOCK_MONOTONIC, &timer_end) == -1) {
+        perror("clock_gettime() failed");
+        exit(errno);
+    }
+    exe_time = BILLION * (timer_end.tv_sec - timer_start.tv_sec) +
+        (timer_end.tv_nsec - timer_start.tv_nsec);
+    printf("elapsed time = %ld ns\n", exe_time);
 }
 
 int main(int argc, char *argv[]) {
-    prepare_files();
+    eval(argv[1]);
 
-    eval();
-
-    write_results();
+    write_result(argv[1]);
 
     printf("Tests failed: %d\n", tests_failed);
     printf("Tests passed: %d\n", tests_passed);
