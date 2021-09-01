bf04012e0abc0d44458dcf48e2e828e7d5f7f173
Jie Zhou <jzhou41@cs.rochester.edu>
2021-09-01 15:56:26 -0400

    [libsafemm] Revised the two Macros for casting checked ptr to raw ptr.
    Added a pair of parenthesis to enhance its robustness.

--- include/safe_mm_checked.h
+++ include/safe_mm_checked.h
@@ -13,7 +13,7 @@
 
 /* Extract the raw pointer from a checked pointer. */
 #define _GETPTR(T, p) ((T *)(p))
-#define _GETARRAYPTR(T, p) ((T *)p)
+#define _GETARRAYPTR(T, p) ((T *)(p))
 
 /* These macros provide convenience for programmers to type a little less. */
 #define MM_ALLOC(T) mm_alloc<T>(sizeof(T))
