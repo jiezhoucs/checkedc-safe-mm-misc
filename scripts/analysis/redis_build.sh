35d22a514b8a3e0f49c32877aa513635f32fb294
Jie Zhou <jzhou41@cs.rochester.edu>
2021-09-28 14:53:43 -0400

    [prog_data] Updated the redis build script.
    There is not a configure for redis. Environment variables go to the
    same line of "make".

--- scripts/analysis/redis_build.sh
+++ scripts/analysis/redis_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# This scripts configures the compiling files of redis-6.2.5
+# This scripts configures and builds redis-6.2.5
 #
 
 . common.sh
@@ -9,15 +9,21 @@
 SRC_DIR="$PROGRAMS/redis-6.2.5"
 BUILD_DIR="$SRC_DIR/build"
 
-export CC="$CC"
-export CXX="$CXX"
-export CFLAGS="-flto"
-export CXXFLAGS="-flto"
-export LD="$CC"
-export LDFLAGS="-fuse-ld=lld $MARSHAL_ARRAY_PASS"
-export AR="$AR"
-export NM="$NM"
+CC="$CC"
+CXX="$CXX"
+CFLAGS="-flto"
+CXXFLAGS="-flto"
+LD="$CC"
+LDFLAGS="-fuse-ld=lld"
+LDFLAGS="$LDFLAGS -lstdc++ $DYN_STATS_PASS $ANALYSIS_LIB/libanalysis.o"
+# LDFLAGS="$LDFLAGS $GET_OBJ_SIZE_PASS"
+AR="$AR"
+NM="$NM"
 
 cd $SRC_DIR
 
-make -j8
+make clean
+make CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" -j8
+make PREFIX="$BUILD_DIR" install
+
+rm -f /tmp/analysis_result.txt
