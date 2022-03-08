a7b9aa674971ae178a3173a27426066bcbdc1946
Jie Zhou <jzhou41@cs.rochester.edu>
2022-03-08 16:55:03 -0500

    [curl] Added the source code of the baseline curl-7.79.1.
    I plan to port the engine and the HTTP part of curl.

---
+++ benchmarks/baseline/curl/curl_build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#
+# This scripts configures the Makefiles for curl-7.79.1.
+# This script is supposed to be invoked in its directory.
+#
+# To run built-in tests, do "make test -jx".
+#
+
+ROOT_DIR=$(realpath ../../../../)
+MISC_DIR=$ROOT_DIR/misc
+
+CC=$ROOT_DIR/llvm-vanilla/build/bin/clang
+CFLAGS="-O3 -Wall"
+
+SRC_DIR=`realpath .`
+BUILD_DIR="$SRC_DIR/build"
+OPENSSL_DIR=/home/linuxbrew/.linuxbrew/Cellar/openssl@1.1/1.1.1l_1
+
+#
+# Run configure to create build files.
+#
+config_configure() {
+    if [[ ! -f "configure" ]]; then
+        autoreconf -fi
+    fi
+
+    cd $SRC_DIR
+    if [[ ! -d $BUILD_DIR ]]; then
+        mkdir $BUILD_DIR
+    fi
+
+    if [[ -f Makefile ]]; then
+        make -k clean
+    fi
+
+    ./configure  --prefix="$BUILD_DIR"                                         \
+                 --disable-shared                                              \
+                 --disable-threaded-resolver                                   \
+                 --with-openssl="$OPENSSL_DIR"                                 \
+                 CC="$CC"                                                      \
+                 CFLAGS="$CFLAGS"                                              \
+}
+
+#
+# Entrance of this script.
+#
+config_configure
+
+if [[ $1 == "make" ]]; then
+    make -j8
+fi
