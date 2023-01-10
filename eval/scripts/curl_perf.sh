d95be111e73822bc756a3d44d23ec94b0c1edd2b
Jie Zhou <jzhou41@cs.rochester.edu>
2023-01-09 22:29:15 -0500

    [curl] Wrote a script to compute Checked C's perf overhead on curl.
    A bash script instead of a python one.

---
+++ eval/scripts/curl_perf.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This script computes the Checked C's performance overhead on curl.
+
+set -e
+
+. common.sh
+
+DATA_DIR="$DATA_DIR/curl"
+
+ITER=20
+
+#
+# Compute the average execution time is:
+#
+perf() {
+    cd $DATA_DIR/baseline
+    baseline=`grep considered * | cut -d' ' -f7 | paste  -sd+ - | bc`
+    baseline=`echo "$baseline/$ITER" | bc`
+    cd ../checked
+    checked=`grep considered * | cut -d' ' -f7 | paste  -sd+ - | bc`
+    checked=`echo "$checked/$ITER" | bc`
+
+    echo "baseline = $baseline s, checked = $checked s"
+
+    result=`echo "scale=4;$checked/$baseline" | bc`
+    echo "Checked C's normalized execution time = $result"
+}
+
+perf
